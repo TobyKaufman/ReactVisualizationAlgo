4b63b258708a354e967643eb90dc54cb97822689
tobykaufman <68293413+TobyKaufman@users.noreply.github.com>
2024-06-23 03:37:48 +0000

    update board

--- src/board.js
+++ src/board.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 export function Board({ height, width }) {
   this.height = height;
   this.width = width;
+  let swag;
   const [speed, setSpeed] = useState(1);
 
   function generateBoard() {}
