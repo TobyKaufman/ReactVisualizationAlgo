18a1c3a65e8cdc4d75361f96a010757ceada77cc
TobyKaufman <kaufman.t@northeastern.edu>
2024-07-04 21:17:42 -0400

    html landing page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>Algorithm Visualizer</h1>
+      <h2>Click on the tiles to make walls</h2>
+      <h3>Click on the button to start the visualization</h3>
       <div className="Board">
         <Board />
         
